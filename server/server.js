dac0d4dc429a860a4fd199bc5a6118ddd2dec99d
Adam <adam@example.com>
2018-06-02 20:05:25 +0200

    feat(mobile): remove old mobile project & start new one via react-native cli

--- server/server.js
+++ server/server.js
@@ -1,14 +1,36 @@
-var express = require('express');
-var https = require('https');
-var http = require('http');
-var fs = require('fs');
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
-var options = {
-  key: fs.readFileSync('./zerossl/key.key'),
-  cert: fs.readFileSync('./zerossl/crt.crt')
-};
+const port = process.env.PORT || 8000;
 
-var app = express();
+// var options = {
+//   key: fs.readFileSync('./zerossl/key.key'),
+//   cert: fs.readFileSync('./zerossl/crt.crt')
+// };
+
+io.on('connection', function (socket) {
+  console.log(socket.id);
+
+  socket.on('toRon', function (data) {
+    console.log('toRon');
+    console.log(data);
+    socket.broadcast.emit('toRon', data);
+  });
+
+  socket.on('toHermione', function (data) {
+    console.log('toHermione');
+    socket.broadcast.emit('toHermione', data);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send("test");
+})
 
 // http.createServer(app).listen(80);
-https.createServer(options, app).listen(443);
+// https.createServer(options, app).listen(8000);
+
+app.listen(port, function() {
+  console.log("Server running on port " + port);
+})
